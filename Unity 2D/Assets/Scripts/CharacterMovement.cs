1b23c13c1a8e3101d64879d46520af80365e8498
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2019-11-16 09:14:17 +1030

    progress is progress

---
+++ Unity 2D/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Rigidbody2D _rigidBody2D;
+
+    private float _movePlayerHorizontal;
+    private float _movePlayerVertical;
+    private Vector2 _movement;
+
+    [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        _rigidBody2D = (Rigidbody2D) GetComponent(typeof(Rigidbody2D));
+    }
+
+    void Update()
+    {
+        _movePlayerHorizontal = Input.GetAxisRaw("Horizontal");
+        _movePlayerVertical = Input.GetAxisRaw("Vertical");
+        _movement = new Vector2(_movePlayerHorizontal, _movePlayerVertical);
+        _rigidBody2D.velocity = _movement * speed;
+    }
+}
---
+++ Unity 2D/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Player : Entity
+{
+    [SerializeField] private string[] inventory;
+    [SerializeField] private string[] skills;
+    [SerializeField] private int money;
+    private Rigidbody2D _rigidBody2D;
+
+    void Awake()
+    {
+        _rigidBody2D = (Rigidbody2D) GetComponent(typeof(Rigidbody2D));
+    }
+    
+}
